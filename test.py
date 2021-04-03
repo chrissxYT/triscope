49dd21088acddebffde16623e9c733ad7f0c4ba0
chrissx <chrissx@chrissx.de>
2021-04-03 23:54:10 +0200

    made the test meter just measure a tri wave like it should

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import json
-import random
 import socket
 import struct
 import time
@@ -9,13 +8,14 @@ import time
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(("127.0.0.1", 14258))
 infojson = json.dumps({'name': 'Test Meter', 'probes': [
-    {'name': 'Bad TSC', 'type': 'time', 'res': 32, 'scale': 1.0},
-    {'name': 'Fake Amps', 'type': 'current', 'res': 32, 'scale': .001}]}).encode('utf-8')
+    {'name': 'Bad TSC', 'type': 'time', 'res': 32, 'scale': .1},
+    {'name': 'Fake Amps', 'type': 'current', 'res': -32, 'scale': .001}]}).encode('utf-8')
 sock.send(struct.pack('>I', len(infojson)))
 sock.send(infojson)
 timestamp = 0
+tri = lambda x: x % 1 * 2 - 1
 while True:
     sock.send(struct.pack('>I', timestamp))
-    sock.send(struct.pack('>I', random.randint(1, 30000)))
+    sock.send(struct.pack('>i', int(tri(timestamp/10)*5000)))
     timestamp += 1
-    time.sleep(1)
+    time.sleep(.1)
